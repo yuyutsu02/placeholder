aaff386db73d93aa863e28b59827be5586039956
Yuyutsu Singh <yuyutsu02@gmail.com>
2026-08-11 21:58:12 +0530

    fixed typos

--- todo/database.py
+++ todo/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base 
 
-SQL_ALCHEMY_DATABASE_URL = "sqlit:///./todo.db"
+SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 
 # engine 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL , connect_args={'check_same_thread':False})
