9806b01b681593c379a64e0abbd92e51fccf453d
Yuyutsu Singh <--global>
2026-09-01 20:12:09 +0530

    Revert "JWT token creation added"
    This reverts commit 5564b78821eff0b2923b84eabcaa20a1fa464094.

--- todo/router/auth.py
+++ todo/router/auth.py
@@ -6,11 +6,7 @@ from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
-from datetime import timedelta , datetime , timezone
 
-SECRET_KEY = "fv83mvH10OVfe65o788io865D3we56rt3y3id774b62J3K3B5796J2Bi4dv438KHjgv0rbBJgfrfVfUeYOb3h"
-ALGO = "HS256"
 
 router = APIRouter()
 bcrypt_context = CryptContext(schemes=['bcrypt'] , deprecated='auto')
@@ -29,16 +25,8 @@ def authenticate_user(username:str , password:str , db):
     if not user:
         return False
     elif(bcrypt_context.verify(password , user.hashed_password)):
-        return user
+        return True
     return False
-
-
-def create_jwt(username:str , user_id:int , expires_delta:timedelta):
-    encode = {'sub': username , 'id': user_id}
-    expires = datetime.now(timezone.utc) + expires_delta
-    encode.update({'exp': expires})
-    return jwt.encode(encode , SECRET_KEY , algorithm= ALGO)
-
 
 class User_Request(BaseModel):
     first_name:str = Field(min_length= 1  , max_length= 20)
@@ -71,8 +59,7 @@ async def get_auth_token(form_data:Annotated[OAuth2PasswordRequestForm , Depends
     user = authenticate_user(form_data.username , form_data.password , db)
 
     if user:
-        token = create_jwt(user.username , user.id , timedelta(minutes=20))
-        return {"access_token":token , "token_type":"bearer"}
+        return "Authenticated"
     else:
         return "Failed Authentication"
         
